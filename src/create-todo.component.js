8fd28eb279cf86d4be70901cfb66402dd9c44033
nitikornchumnankul <nitikornchumnankul@gmail.com>
2019-05-16 22:48:44 +0700

    I have created constructor and function already

--- src/create-todo.component.js
+++ src/create-todo.component.js
@@ -1,7 +1,52 @@
-import React, {Component} from 'react';
-export default class CreateTodo extends Component{
-    render(){
-        return(
+import React, { Component } from 'react';
+export default class CreateTodo extends Component {
+    constructor(props) {
+        super(props);
+        this.onChangeTodoDescription = this.onChangeTodoDescription.bind(this);
+        this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
+        this.onChangeTodoPriority    = this.onChangeTodoPriority.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            todo_description: '',
+            todo_responsible: '',
+            todo_priority: '',
+            todo_completed: false
+        }
+    }
+    onChangeTodoDescription(e) {
+        this.setState({
+            todo_description: e.target.value
+        });
+
+    }
+    onChangeTodoResponsible(e) {
+        this.setState({
+            todo_responsible: e.target.value
+        });
+    }
+    onChangeTodoPriority(e) {
+        this.setState({
+            todo_priority: e.target.value
+        });
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        console.log('From submitted:');
+
+        console.log('Todo Description : ${this.state.todo_description}');
+        console.log('Todo Tesponsible: ${this.state.todo_responsible}');
+        console.log('Todo Priority: ${this.state.todo_priority}');
+
+        this.setState({
+            todo_description: '',
+            todo_responsible: '',
+            todo_priority: '',
+            todo_completed: false
+        })
+    }
+    render() {
+        return (
             <div>
                 <p>
                     Welcome to Create Todo Component!!
