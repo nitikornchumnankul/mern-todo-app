55b5190317d323f66267bb59711c270ffc74beb5
nitikornchumnankul <nitikornchumnankul@gmail.com>
2019-05-16 20:21:55 +0700

    I can show logo on my page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import CreateTodo from "./create-todo.component";
 import EditTodo from "./edit-todo.component";
 import TodosList from "./todos-list.component";
+import github from "./github-512.png";
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="container">
-
-          <h2>MERN-Stack Todo App</h2>
-          <br />
-          <Route path="/" exact component={TodosList} />
-          <Route path="/edit/:id" component={EditTodo} />
-          <Route path="/create" component={CreateTodo} />
+          <nav className="navbar navbar-expand-lg navbar-light be-light">
+            <a className="navbar-brand" href="https://codingthesmartway.com" target="">
+              <img src={github} width="30" height="30" alt="CodingTheSmartWay.com"></img>
+            </a>
+            <Link to="/" className="navbar-brand" >MERN-Stack Todo App</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+            <Route path="/" exact component={TodosList} />
+            <Route path="/edit/:id" component={EditTodo} />
+            <Route path="/create" component={CreateTodo} />
         </div>
-      </Router>
+      </Router >
     );
   }
 }
